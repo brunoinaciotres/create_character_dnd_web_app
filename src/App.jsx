fe67392dc4a26020a2e27d36580b7313fa3e8bdf
Bruno <btres76@gmail.com>
2024-07-13 12:57:38 -0300

    create first screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,74 +5,32 @@ export default function App() {
   return (
     <main className='main-container'>
       <div className="page-header">
-        <h1 className='main-card-title mb-1'>Criar Personagem</h1>
-        <div className="main-card-subtitle">
-          Escolha uma raça
+        <h1 className='title '>Criar Personagem</h1>
+        <div className="subtitle">
+          D&D 5E
         </div>
+      </div>
+      <div className="step-description">
+        <div className="title">Passo 1</div>
+        <div className="subtitle">Defina raça, classe e background</div>
       </div>
       <div className="list-item-group">
         <div className="list-item">
-          <div className="wave"></div>
-          <div className="list-item-text">
-            Anão
-          </div>
-
+          <span>Raça</span>
+          <a class="grey-link">Escolher</a>
         </div>
         <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Elfo
-        </div>
-
-          </div>
-        <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Halfling
-
-          </div>
+          <span>Classe</span>
+          <a class="grey-link">Escolher</a>
         </div>
         <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Humano
+          <span>Background</span>
+          <a class="grey-link">Escolher</a>
         </div>
-
-          </div>
-        <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Draconato
-
-          </div>
-        </div>
-        <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Gnomo
-        </div>
-
-          </div>
-        <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Meio-Elfo
-
-          </div>
-        </div>
-        <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Meio-Orc
-
-          </div>
-        </div>
-        <div className="list-item">
-        <div className="wave"></div>
-          <div className="list-item-text">
-          Tiefling
-
-          </div>
+      </div>
+      <div className="footer">
+        <div className="disabled-button">
+          Defina as opções
         </div>
       </div>
     </main>
